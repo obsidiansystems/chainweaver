619c548b82a6f8bc933656693a56393abcda4b6e
Carlo Nucera <meditans@gmail.com>
2022-02-05 00:36:04 +0100

    More of the new static pattern

--- frontend/src/Frontend/Setup/Widgets.hs
+++ frontend/src/Frontend/Setup/Widgets.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -46,6 +47,6 @@ continueForm = setupForm "continue-button" "Continue"
 -- | Wallet logo
 kadenaWalletLogo :: DomBuilder t m => m ()
 kadenaWalletLogo = divClass "logo" $ do
-  elAttr "img" ("src" =: static @"img/kadena_blue_logo.png" <> "class" =: setupClass "kadena-logo") blank
+  elAttr "img" ("src" =: $(static "img/kadena_blue_logo.png") <> "class" =: setupClass "kadena-logo") blank
   elClass "div" "chainweaver" $ text "Chainweaver"
   elClass "div" "by-kadena" $ text "by Kadena (BETA)"
